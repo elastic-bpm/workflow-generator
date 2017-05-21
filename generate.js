a7980749d89fc73aec445fcd54690e081be05c08
Johannes Bertens <johannes.bertens@gmail.com>
2017-05-21 16:28:39 +0200

    Using poisson arrival times now

--- generate.js
+++ generate.js
@@ -43,27 +43,22 @@ var sampleFlows = [
 ];
 
 var workflows = [];
-var maxDelay = program.duration*60*1000;
+var durationInMS = program.duration*60*1000;
+var avgInterval = durationInMS / program.amount;
 var r = new Random(Random.engines.mt19937().seed(program.seed));
-var minDelay = maxDelay+1; // Init to high value
 
 for (var i = 0; i < program.amount; i++) {
     var flow = JSON.parse(JSON.stringify(r.pick(sampleFlows))); // Deep clone
-    flow.delay = r.integer(0, maxDelay);
     flow.owner = sampleOwner;
-    if (flow.delay < minDelay) minDelay = flow.delay;
+    flow.delay = 0;
+    if (i > 0) {
+        // Using poisson arrival times now
+        flow.delay = parseInt(-Math.log(r.real(0,1)) * avgInterval) + workflows[i-1].delay;
+    }
+
+    flow.name = "Workflow with " + flow.nodes.split(',').length + " nodes starting at " + flow.delay;
     workflows.push(flow);
 }
-
-// Let it start right away & set name
-for (var i = 0; i < workflows.length; i++) {
-    workflows[i].delay = workflows[i].delay - minDelay;
-    workflows[i].name = "Workflow with " + workflows[i].nodes.split(',').length + " nodes starting at " + workflows[i].delay;
-}
-
-workflows.sort(function(a, b) {
-    return a.delay - b.delay;
-});
 
 fs.writeFile(program.output, JSON.stringify(workflows, null, 2), function(){
     console.log("Done!");
