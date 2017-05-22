b813c8dcd01fcd30daf80199efb426b364096900
Johannes Bertens <johannes.bertens@gmail.com>
2017-05-22 21:04:18 +0200

    Workflows

--- saveOutput.r
+++ saveOutput.r
@@ -1,14 +1,19 @@
 library("rjson")
 setwd("C:\\Users\\Johannes\\Projects\\elastic\\workflow-generator")
 
-json_data <- fromJSON(file="output.json")
-
-json_data[3][[1]]$delay
-
-delays <- numeric()
+json_data <- fromJSON(file="50.json")
+delays50 <- numeric()
 for (i in json_data) {
-  delays <- c(delays, i$delay / 1000)
+  delays50 <- c(delays50, i$delay / 1000)
 }
-print(delays)
-stripchart(delays, xlab="Delay in seconds", pch="|")
-abline(h=1)
+
+json_data <- fromJSON(file="100.json")
+delays100 <- numeric()
+for (i in json_data) {
+  delays100 <- c(delays100, i$delay / 1000)
+}
+
+x <- list("50 wfs"=delays50, "100wfs"=delays100)
+stripchart(x, xlab="Delay in seconds", pch="|", at=c(1.2,1.7))
+abline(h=1.2)
+abline(h=1.7)
